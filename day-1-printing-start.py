cecc489885b23eaa69489172b4ff8d85a7af94ba
zeljko5erlik <zeljko.peterlik@gmail.com>
2024-01-05 00:12:47 +0100

    Day 1 end

--- day-1-printing-start.py
+++ day-1-printing-start.py
@@ -1,3 +1,5 @@
-# print('Hello ' + 'Zeljko')
+print('Welcome to the Band Name Generator.')
+grew_up_city_name = input("What's the name of the city you grew up in?\n")
+grew_up_pet_name = input('Whats your pet name?\n')
 
-print('Hello ' + input('What is your name?') + '!')
+print(f'Your band name could be: {grew_up_city_name} {grew_up_pet_name}')
